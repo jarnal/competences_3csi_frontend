1b63e171a46129e28ca245b40e44234d7aa81ec0
Thomas Sebbane <thomas.sebbane@hotmail.fr>
2016-03-28 22:55:20 +0200

    Update

--- routes/Dashboard/index.js
+++ routes/Dashboard/index.js
@@ -1,16 +1,5 @@
 module.exports = {
   path: 'dashboard',
-
-  /*getChildRoutes(location, cb) {
-    require.ensure([], (require) => {
-      cb(null, [
-        require('./routes/Announcements'),
-        require('./routes/Assignments'),
-        require('./routes/Grades')
-      ])
-    })
-  },*/
-
   getComponent(location, cb) {
     require.ensure([], (require) => {
       cb(null, require('./components/Dashbord'))
