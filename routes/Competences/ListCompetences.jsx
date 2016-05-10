d29230d83d775737685812f85478e1267d37bd12
jonathan <jonathan.arnal89@gmail.com>
2016-05-10 10:29:30 +0200

    @update: chargement des matieres et des competences depuis la page compétences.

--- routes/Competences/ListCompetences.jsx
+++ routes/Competences/ListCompetences.jsx
@@ -1,141 +1,122 @@
-import React from 'react'
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
-import Select from 'react-select'
+    import React from 'react'
+    import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+    import Select from 'react-select'
 
-
-const Competences = [];
-
-function addCompetences(quantity) {
-  const startId = Competences.length;
-  for (let i = 0; i < quantity; i++) {
-    const id = startId + i;
-    Competences.push({
-      id: id,
-      name: 'Nom de la Competence ' + id,
-      price: 2100 + i
-    });
-  }
-}
-
-addCompetences(50);
-
-const selectRowProp = {
-  mode: 'checkbox',
-  clickToSelect: true  // enable click to select
-};
-
-const options_table = {
-  noDataText: "Aucune competence trouvée"
-};
-
-var ListClass = React.createClass({
-  getInitialState: function() {
-    return {
-      ListClass: 'Chargement...'
+    const selectRowProp = {
+      mode: 'checkbox',
+      clickToSelect: true
     };
-  },
 
-  componentDidMount: function() {
-    this.serverRequest = $.get(this.props.source, function (result) {
-      console.log(result.class[0]);
-      var ListClass = result.class[0];
-      this.setState({
-        ListClass: ListClass
-      });
-    }.bind(this));
-  },
+    const options_table = {
+      noDataText: "Aucune competence trouvée"
+    };
 
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
+    var options = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' }
+    ];
 
-  render: function() {
-    return (
-      <div>
-        {this.state.ListClass}
-      </div>
-    );
-  }
-});
+    function logChange(val) {
+        console.log("Selected: " + val);
+    }
 
-var options = [
-    { value: 'one', label: 'One' },
-    { value: 'two', label: 'Two' }
-];
+    var ListCompetences = React.createClass({
 
-function logChange(val) {
-    console.log("Selected: " + val);
-}
+      //
+      getInitialState () {
+        return {
+          multi: false,
+          competences: []
+        };
+      },
 
-class ListCompetences extends React.Component {
+      //
+      onChange (value) {
+        console.log(value);
+        this.setState({
+          value: value
+        });
+        this.getCompetences(value.id);
+      },
 
-  render() {
-    return (
-      <div>
-        <Select
-          name="form-field-name form-control select2"
-          value="one"
-          options={options}
-          onChange={logChange}
-          />
+      //
+      getCompetences: function(matiereID){
+        $.get(
+            "http://192.168.100.10/web/app_dev.php/api/matiere/"+ matiereID +"/competences?access_token=NDIyODJjYzg4OTQ2NjAyMzE1OTkyZjkzYmFkY2IyYzE4MzNmNDJmNjI1MzY1ZDM3MWVlNTA3ZjA0YmFlNWFhMQ",
+            function (result) {
+              this.setState({
+                competences: result
+              });
+            }.bind(this)
+        );
+      },
 
-        <ListClass source="http://localhost:8081/class" />
+      //
+      getMatieres: function(input, callback) {
+        $.get(
+            "http://192.168.100.10/web/app_dev.php/api/matiere/?access_token=NDIyODJjYzg4OTQ2NjAyMzE1OTkyZjkzYmFkY2IyYzE4MzNmNDJmNjI1MzY1ZDM3MWVlNTA3ZjA0YmFlNWFhMQ",
+            function (result) {
+              var data = {
+                options: result["matieres"],
+                complete: false
+              };
+              callback(null, data);
+            }.bind(this)
+        );
+      },
 
-        {/* Left col */}
-        <section className="col-lg-7 connectedSortable">
-          {/* Listes Competences */}
-          <div className="box box-primary">
-            <div className="box-header with-border">
-              <h3 className="box-title">Choix dun groupe de compétences</h3>
-              <div className="col-md-3 col-xs-3 col-lg-3 pull-right">
-              </div>
-            </div>{/* /.box-header */}
-            <div className="box-body">
-              <div className="row">
-                <div className="col-md-11 col-xs-12 col-lg-11">
-                  <div className="form-group">
-                    <select className="form-control select2" style={{width: '100%'}}>
-                      <option>PHP Initiation</option>
-                      <option>PHP Avancé</option>
-                      <option>C++ Programation</option>
-                      <option>Réseau</option>
-                      <option>Java</option>
-                      <option>Module Développeur</option>
-                      <option>Module Réseau</option>
-                    </select>
-                  </div>{/* /.form-group */}
-                </div>{/* /.col */}
-                <div className="col-md-1 col-xs-12 col-lg-1">
-                  <div className="form-group">
-                    <button className="btn btn-default pull-right"><i className="fa fa-plus" /> </button>
-                  </div>{/* /.form-group */}
-                </div>{/* /.col */}
-                <div className="col-md-12 col-xs-12 col-lg-12">
-                  <BootstrapTable
-                    data={Competences}
-                    height="250"
-                    striped={true}
-                    hover={true}
-                    selectRow={selectRowProp}
-                    searchPlaceholder="Rechercher"
-                    search={true}
-                    noDataText="test"
-                    options={options_table}>
-                    <TableHeaderColumn dataField="id" isKey={true} dataSort={true} hidden={true}>Competence ID</TableHeaderColumn>
-                    <TableHeaderColumn dataField="name" dataSort={true}>Nom de la compétence</TableHeaderColumn>
-                  </BootstrapTable>
+      //
+      render: function() {
+        return (
+          <div>
 
+            {/* Left col */}
+            <section className="col-lg-7 connectedSortable">
+              {/* Listes Competences */}
+              <div className="box box-primary">
+                <div className="box-header with-border">
+                  <h3 className="box-title">Choix dun groupe de compétences</h3>
+                  <div className="col-md-3 col-xs-3 col-lg-3 pull-right">
+                  </div>
+                </div>{/* /.box-header */}
+                <div className="box-body">
+                  <div className="row">
+                    <div className="col-md-11 col-xs-12 col-lg-11">
+                      <div className="form-group">
+                        <Select.Async value={this.state.value} onChange={this.onChange} valueKey="id" labelKey="name" loadOptions={this.getMatieres} />
+                      </div>{/* /.form-group */}
+                    </div>{/* /.col */}
+                    <div className="col-md-1 col-xs-12 col-lg-1">
+                      <div className="form-group">
+                        <button className="btn btn-default pull-right"><i className="fa fa-plus" /> </button>
+                      </div>{/* /.form-group */}
+                    </div>{/* /.col */}
+                    <div className="col-md-12 col-xs-12 col-lg-12">
+                      <BootstrapTable
+                        data={this.state.competences}
+                        height="250"
+                        striped={true}
+                        hover={true}
+                        selectRow={selectRowProp}
+                        searchPlaceholder="Rechercher"
+                        search={true}
+                        noDataText="test"
+                        options={options_table}>
+                        <TableHeaderColumn dataField="id" isKey={true} dataSort={true} hidden={true}>Competence ID</TableHeaderColumn>
+                        <TableHeaderColumn dataField="name" dataSort={true}>Nom de la compétence</TableHeaderColumn>
+                      </BootstrapTable>
+
+                    </div>
+                  </div>{/* /.row */}
+                </div>{/* /.box-body */}
+                <div className="box-footer clearfix no-border">
+                  <button className="btn btn-default col-md-12 col-xs-12">Ajouter une compétence</button>
                 </div>
-              </div>{/* /.row */}
-            </div>{/* /.box-body */}
-            <div className="box-footer clearfix no-border">
-              <button className="btn btn-default col-md-12 col-xs-12">Ajouter une compétence</button>
-            </div>
-          </div>{/* /.box */}
-        </section>{/* /.Left col */}
-      </div>
-    )
-  }
-}
+              </div>{/* /.box */}
+            </section>{/* /.Left col */}
+          </div>
+        )
+      }
+    });
 
-export default ListCompetences
+    export default ListCompetences
--- routes/auth/Auth.jsx
+++ routes/auth/Auth.jsx
@@ -1,49 +1,120 @@
-module.exports = {
-  login(nom, mdp, cb) {
-    cb = arguments[arguments.length - 1]
-    if (localStorage.token) {
-      if (cb) cb(true)
-      this.onChange(true)
-      return
+var SERVER_BASE_URL = "http://192.168.100.10/web/app_dev.php/api/";
+var SERVER_OAUTH_URL = "http://192.168.100.10/web/app_dev.php/oauth/v2/token";
+
+/**
+ *
+ */
+function login(nom, mdp, callback) {
+
+  var ajaxCall = $.ajax({
+    type: "POST",
+    url: SERVER_BASE_URL + "security",
+    dataType: "json",
+    data: {
+      user_l: nom,
+      user_p: mdp
+    },
+    success:function(pResponse){
+      localStorage.setItem('us_id', pResponse.user_id);
+      localStorage.setItem('us_api_key', pResponse.api_key);
+      localStorage.setItem('us_name', pResponse.user_name);
+      localStorage.setItem('us_role', pResponse.user_role);
+
+      retrieveTokenFromServer(callback);
+    },
+    error:function(error){
+      console.log(error);
     }
-    pretendRequest(nom, mdp, (res) => {
-      if (res.authenticated) {
-        localStorage.token = res.token
-        if (cb) cb(true)
-        this.onChange(true)
-      } else {
-        if (cb) cb(false)
-        this.onChange(false)
-      }
-    })
-  },
-
-  getToken() {
-    return localStorage.token
-  },
-
-  logout(cb) {
-    delete localStorage.token
-    if (cb) cb()
-    this.onChange(false)
-  },
-
-  loggedIn() {
-    return !!localStorage.token
-  },
-
-  onChange() {}
+  });
 }
 
-function pretendRequest(nom, mdp, cb) {
-  setTimeout(() => {
-    if (nom === 'admin' && mdp === 'admin') {
-      cb({
-        authenticated: true,
-        token: Math.random().toString(36).substring(7)
-      })
-    } else {
-      cb({ authenticated: false })
+/**
+ *
+ */
+function logout() {
+  delete localStorage.tok
+  if (cb) cb()
+    this.onChange(false)
+}
+
+/**
+ *
+ */
+function retrieveTokenFromServer(callback) {
+  $.ajax({
+    type: "GET",
+    url: SERVER_OAUTH_URL,
+    dataType: "json",
+    data: {
+      client_id: "1_2lp2nq4n5acksksg8ks4w80gskg8kockggwg08w008so8gkw4s",
+      client_secret:"22b4bent9e74ggggss8wgk0s0o88w408sgw8oksggw4c000cs",
+      grant_type: "http://competences3csi.com/grants/api_key",
+      api_key: localStorage.getItem('us_api_key')
+    },
+    success:function( pResponse ){
+      localStorage.setItem('tok', pResponse.access_token);
+      localStorage.setItem('ref_tok', pResponse.refresh_token);
+
+      callback(true);
+    },
+    error:function(error){
+      console.log(error);
     }
-  }, 0)
+  });
+}
+
+/**
+ *
+ */
+function getUserInfo() {
+  return {
+      "user_id": localStorage.getItem('us_id'),
+      "user_name": localStorage.getItem('us_name'),
+      "user_role": localStorage.getItem('us_role')
+  }
+}
+
+/**
+ *
+ */
+function getToken(){
+  return localStorage.getItem("tok");
+}
+
+/**
+ *
+ */
+function loggedIn() {
+  return !!getToken();
+}
+
+/**
+ *
+ */
+function getUsers()
+{
+    $.ajax({
+        type: "GET",
+        url: "http://192.168.100.10/web/app_dev.php/api/matiere/",
+        dataType: "json",
+        data: {
+            "access_token": localStorage.getItem('tok')
+        },
+        success:function( pResponse ){
+            console.log(pResponse);
+        },
+        error:function(error){
+            console.log(error);
+        }
+    });
+}
+
+module.exports = {
+
+  login: login,
+  logout: logout,
+  retrieveTokenFromServer: retrieveTokenFromServer,
+  getToken: getToken,
+  loggedIn: loggedIn,
+  onChange() {}
 }
