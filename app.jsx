afdf72815354ea7b1273040567ca3a0d3a2eb0d4
Menesys <camille.vasseur1995@gmail.com>
2016-05-15 17:36:48 +0200

    Mise en forme de l'écran Login

--- app.jsx
+++ app.jsx
@@ -63,15 +63,37 @@ const Login = withRouter(
         },
 
         render() {
+
+          var style = {color: 'white'};
+
             return (
-                <form onSubmit={this.handleSubmit}>
-                    <label><input ref="email" placeholder="email" defaultValue="joe@example.com"/></label>
-                    <label><input ref="pass" placeholder="password"/></label> (hint: password1)<br />
-                    <button type="submit">login</button>
-                    {this.state.error && (
-                        <p>Bad login information</p>
-                    )}
-                </form>
+              <div className="login-box">
+                <div className="login-logo" style={style}>
+                  <b>Admin</b>Skill
+                </div>
+                <div className ="login-box-body">
+                <p className="login-box-msg">Sign in to start your session</p>
+                  <form onSubmit={this.handleSubmit}>
+                    <div className="form-group has-feedback">
+                      <input className="form-control" placeholder="Email" ref="email"/>
+                      <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    </div>
+
+                    <div className="form-group has-feedback">
+                      <input className="form-control" type="password" placeholder="Password" ref="pass"/>
+                      <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <br/>
+                    <div>
+                      <button className="btn btn-primary btn-block btn-flat" type="submit">Login</button>
+                      {this.state.error && (
+                          <p>Bad login information</p>
+                      )}
+                    </div>
+                    <br/>
+                  </form>
+                </div>
+              </div>
             )
         }
     })
@@ -83,7 +105,16 @@ const Logout = React.createClass({
     },
 
     render() {
-        return <p>You are now logged out</p>
+        return(
+          <div className="login-box">
+            <div className="login-logo" style={{color: 'white'}}>
+              <b>Admin</b>Skill
+            </div>
+            <div className ="login-box-body">
+              <p className="login-box-msg">You are now logged out</p>
+            </div>
+          </div>
+        )
     }
 });
 render(
